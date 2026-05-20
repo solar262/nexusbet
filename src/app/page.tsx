959f49ad16699665fafe7cc9ac440cb33ae74922
solar262 <solardynamics592@gmail.com>
2026-05-20 13:01:35 +0200

    Fix typescript and force dynamic build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 import LobbyClient from './LobbyClient';
 
+export const dynamic = 'force-dynamic';
+
 // We must instantiate the prisma client securely on the server
 const prisma = new PrismaClient();
 
